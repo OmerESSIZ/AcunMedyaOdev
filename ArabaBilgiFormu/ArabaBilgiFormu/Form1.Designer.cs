a868a58ecd323cb18d86efb99de3f46d435c8055
OmerESSIZ <148744035+OmerESSIZ@users.noreply.github.com>
2025-04-17 11:48:43 +0300

    ARABA

---
+++ ArabaBilgiFormu/ArabaBilgiFormu/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace ArabaBilgiFormu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMarka = new System.Windows.Forms.TextBox();
+            this.txtModel = new System.Windows.Forms.TextBox();
+            this.txtRenk = new System.Windows.Forms.TextBox();
+            this.txtKapiSayisi = new System.Windows.Forms.TextBox();
+            this.txtPencereSayisi = new System.Windows.Forms.TextBox();
+            this.txtYakitTuketimi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMarka
+            // 
+            this.txtMarka.Location = new System.Drawing.Point(342, 105);
+            this.txtMarka.Name = "txtMarka";
+            this.txtMarka.Size = new System.Drawing.Size(100, 26);
+            this.txtMarka.TabIndex = 0;
+            // 
+            // txtModel
+            // 
+            this.txtModel.Location = new System.Drawing.Point(342, 137);
+            this.txtModel.Name = "txtModel";
+            this.txtModel.Size = new System.Drawing.Size(100, 26);
+            this.txtModel.TabIndex = 1;
+            // 
+            // txtRenk
+            // 
+            this.txtRenk.Location = new System.Drawing.Point(342, 166);
+            this.txtRenk.Name = "txtRenk";
+            this.txtRenk.Size = new System.Drawing.Size(100, 26);
+            this.txtRenk.TabIndex = 2;
+            // 
+            // txtKapiSayisi
+            // 
+            this.txtKapiSayisi.Location = new System.Drawing.Point(342, 201);
+            this.txtKapiSayisi.Name = "txtKapiSayisi";
+            this.txtKapiSayisi.Size = new System.Drawing.Size(100, 26);
+            this.txtKapiSayisi.TabIndex = 3;
+            // 
+            // txtPencereSayisi
+            // 
+            this.txtPencereSayisi.Location = new System.Drawing.Point(342, 233);
+            this.txtPencereSayisi.Name = "txtPencereSayisi";
+            this.txtPencereSayisi.Size = new System.Drawing.Size(100, 26);
+            this.txtPencereSayisi.TabIndex = 4;
+            // 
+            // txtYakitTuketimi
+            // 
+            this.txtYakitTuketimi.Location = new System.Drawing.Point(342, 265);
+            this.txtYakitTuketimi.Name = "txtYakitTuketimi";
+            this.txtYakitTuketimi.Size = new System.Drawing.Size(100, 26);
+            this.txtYakitTuketimi.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(270, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "MARKA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(272, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "MODEL";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(285, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "RENK";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(233, 207);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "KAPI SAYISI";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(194, 242);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(144, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "PENCERE SAYISI";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(203, 268);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(133, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "YAKIT TÜKETİMİ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(503, 268);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 72);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Bilgileri Göster";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtYakitTuketimi);
+            this.Controls.Add(this.txtPencereSayisi);
+            this.Controls.Add(this.txtKapiSayisi);
+            this.Controls.Add(this.txtRenk);
+            this.Controls.Add(this.txtModel);
+            this.Controls.Add(this.txtMarka);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMarka;
+        private System.Windows.Forms.TextBox txtModel;
+        private System.Windows.Forms.TextBox txtRenk;
+        private System.Windows.Forms.TextBox txtKapiSayisi;
+        private System.Windows.Forms.TextBox txtPencereSayisi;
+        private System.Windows.Forms.TextBox txtYakitTuketimi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ArabaBilgiFormu/ArabaBilgiFormu/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArabaBilgiFormu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string marka = txtMarka.Text;
+            string model = txtModel.Text;
+            string renk = txtRenk.Text;
+            string kapiSayisi = txtKapiSayisi.Text;
+            string pencereSayisi = txtPencereSayisi.Text;
+            string yakitTuketimi = txtYakitTuketimi.Text;
+
+            // Girilen bilgileri MessageBox'ta göster
+            MessageBox.Show($"Marka: {marka}\nModel: {model}\nRenk: {renk}\nKapı Sayısı: {kapiSayisi}\nPencere Sayısı: {pencereSayisi}\n100 km'de Yaktığı Yakıt: {yakitTuketimi} Litre", "Araba Bilgileri");
+        }
+    }
+}
+
